8fd0fa36fcc37212924d6a2f144e130a17d76cfa
Stefan <stefan@jerpa.net>
2020-12-18 06:50:02 +0100

    Day 18

---
+++ day18-1/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	c "adventofcode2020/common"
+	"fmt"
+	"strings"
+)
+
+func calc(s string) int {
+	s = " " + s + " "
+	p := strings.Index(s, "(")
+	if p != -1 {
+		pC := 1
+		var e int
+		for e = p + 1; e < len(s) && pC > 0; e++ {
+			if s[e] == '(' {
+				pC++
+			}
+			if s[e] == ')' {
+				pC--
+			}
+		}
+		v := calc(s[p+1 : e-1])
+
+		s = s[:p-1] + fmt.Sprintf(" %d ", v) + s[e+1:]
+		for strings.Index(s, "(") > -1 {
+			s = fmt.Sprintf("%d", calc(s))
+		}
+	}
+	s = strings.TrimSpace(s)
+	v := strings.Split(s, " ")
+	num := c.GetInt(v[0])
+	for i := 1; i < len(v); i += 2 {
+		if v[i] == "+" {
+			num += c.GetInt(v[i+1])
+		} else if v[i] == "*" {
+			num *= c.GetInt(v[i+1])
+		}
+	}
+	return num
+}
+
+func main() {
+	sum := 0
+
+	for _, v := range c.ReadInputFile() {
+		d := calc(v)
+		//c.Print(d)
+		sum += d
+	}
+	c.Print(sum)
+
+}
